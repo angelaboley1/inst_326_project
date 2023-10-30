3117b44b76d93ce11750362835804dfc94386090
Josie Whittington <jmwhittington10@gmail.com>
2023-10-30 12:49:32 -0400

    started Player class (init and player_turn)

--- inst326_final.py
+++ inst326_final.py
@@ -1,2 +1,14 @@
 
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
 
+    def player_turn(self, card, game):
+        played_cards = []
+        if card in self.hand:
+            self.hand = [c for c in self.hand if c != card]
+            game.played_cards.append(card)
+            print(f"{self.name} played {card}.")
+        else:
+            print(f"{self.name} does not have a {card}.")
