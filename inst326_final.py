e584997057b7b3dee6834e5c386939c52bde27ee
Josie Whittington <jmwhittington10@gmail.com>
2023-11-02 22:10:58 -0400

    update player_turn

--- inst326_final.py
+++ inst326_final.py
@@ -10,10 +10,10 @@ class Player:
         self.hand = []
 
     def player_turn(self, card, game):
-        played_cards = []
+        played_cards = [] 
         if card in self.hand:
             self.hand = [c for c in self.hand if c != card]
-            game.played_cards.append(card)
+            played_cards.append(card)
             print(f"{self.name} played {card}.")
         else:
             print(f"{self.name} does not have a {card}.")
