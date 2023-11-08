dc2c5224c8491582bff605bb3d1e0894fa134183
Erin Keane <eringkeane@gmail.com>
2023-11-08 16:55:06 -0500

    Erin Keane Nov 10th update

--- inst326_final.py
+++ inst326_final.py
@@ -22,8 +22,32 @@ class Computer:
     def __init(self, computer):
         self.computer = computer
 
-class Cards:
-    def __init__(self, color, number, wild):
-        self.color = color 
-        self.number = number 
-        self.wild = wild 
+def deck_info(self):
+    color = ["Red", "Yellow", "Blue", "Green"]
+    type = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Reverse', 'Draw Two', 'Skip']
+    wild = ['Wild Card', 'Draw Four']
+    deck = []
+    
+    for colors in color:
+        for types in type:
+            card_info = "{} {}".format(colors, types)
+            deck.append(card_info)
+        for i in range(4):
+            deck.append([wild[0]]) 
+            
+        return deck
+        
+    def shuffle_deck(deck):
+        random.shuffle_deck(deck)
+   #i think this is how to shuffle the deck through the random function
+
+
+def discard_pile():
+    discard_pile = [ ]
+    for #playerturn in #turn function:
+        discard_pile.append[#card]
+    if len(discard_pile) == 112:
+        #reshuffle 
+   
+   
+   
