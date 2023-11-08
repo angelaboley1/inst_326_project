6f84fc32db3955f1486d4260946199996d40880f
Katy Lamb <katylamb@umd.edu>
2023-11-08 13:33:43 -0500

    katy's nov 10th functions

--- inst326_final.py
+++ inst326_final.py
@@ -49,3 +49,51 @@ def can_play_card(hand, top_of_deck):
     if can_play_card(hand, top_of_deck):
     else:
         raise ValueError("Player must draw a card")
+    
+
+#katy lamb nov. 10th update
+
+#end_game func used sample dict:
+player_hands = {
+    "player1": ["2", "3", "7"],
+    "player2": [],
+    "player3": ["5", "9", "Skip"]
+}
+
+def end_game(dict):
+    """Calculates each player's final score.
+    
+    Args:
+        dict (dict): keys = players' names, 
+            values = list of cards left in players' hands.
+    
+    Returns:
+        list: list of tuples -- (player, final score), sorted by final score.
+    """
+    final_scores = {}
+    score = 0
+    for player in dict:
+        if len(dict[player]) >= 1:
+            for card in dict[player]:
+                score += int(cards.card_points[card])
+        else:
+            score = 0
+        final_scores[player] = score
+    ranks = sorted(final_scores.items(), key= lambda c: c[1])
+    return ranks
+
+
+def print_ranks(list): #should use list returned by end_game func
+    """Prints out player place and their final score.
+    
+    Args:
+        list (list): list of tuples -- (player, final score), 
+            sorted by final score.
+    
+    Side effects:
+        prints to stdout.
+    """
+    place = 0
+    for player in list:
+        place += 1
+        print(f"{place}. {player[0]} with {player[1]} points")
