0f714744611ecccc2dd0d86c90ad0026b4ae1571
Katy Lamb <katylamb@umd.edu>
2023-11-08 20:28:48 -0500

    re-added everyone's commits -- they were deleted in last commit

--- inst326_final.py
+++ inst326_final.py
@@ -22,24 +22,24 @@ class Computer:
     def __init(self, computer):
         self.computer = computer
 
+#Erin Nov.10th
 class Cards:
     def __init__(self, color, number, wild):
         self.color = color 
         self.number = number 
         self.wild = wild 
-def deck_info(self):
-    color = ["Red", "Yellow", "Blue", "Green"]
-    type = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Reverse', 'Draw Two', 'Skip']
-    wild = ['Wild Card', 'Draw Four']
-    deck = []
-
-    for colors in color:
-        for types in type:
-            card_info = "{} {}".format(colors, types)
-            deck.append(card_info)
-        for i in range(4):
-            deck.append([wild[0]]) 
-
+        
+    def deck_info(self):
+        color = ["Red", "Yellow", "Blue", "Green"]
+        type = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Reverse', 'Draw Two', 'Skip']
+        wild = ['Wild Card', 'Draw Four']
+        deck = []
+        for colors in color:
+            for types in type:
+                card_info = "{} {}".format(colors, types)
+                deck.append(card_info)
+            for i in range(4):
+                deck.append([wild[0]]) 
         return deck
 
     def shuffle_deck(deck):
@@ -48,8 +48,72 @@ def deck_info(self):
 
 
 #def discard_pile():
-    #iscard_pile = [ ]
+    #discard_pile = [ ]
     #for #playerturn in #turn function:
         #discard_pile.append[#card]
     #if len(discard_pile) == 112:
-        #reshuffle 
+        #shuffle_deck(discard_pile)
+
+#My Nov.10th
+def can_play_card(hand, top_of_deck):
+    '''
+    checks if any card in hand can be played given the card on top of the deck
+    '''
+    for card in hand:
+        if card.color == top_of_deck.color:
+            return True
+        if card.rank == top_of_deck.rank:
+            return True
+        if "Wild" in card.rank:
+            return True
+        else:
+            return False
+
+#a function about playing card
+    if can_play_card(hand, top_of_deck):
+        ...
+    else:
+        raise ValueError("Player must draw a card")
+
+
+#Angela Nov.10th
+class GameState:
+    def __init__(self, players_name, players_cards, top_card, ):
+        self.players_names = players_name
+        self.players_card = players_cards
+        self.top_card = top_card
+    def __str__(self):
+        return f"{self.players_name} cards: {self.players_cards} \n
+                The top card is {self.top_card} \n 
+                {self.players_name}, it's your turn! What do you want to play?"
+
+
+#Katy Nov.10th
+#end_game func used sample dict:
+player_hands = {
+    "player1": ["2", "3", "7"],
+    "player2": [],
+    "player3": ["5", "9", "Skip"]
+}
+
+def end_game(dict):
+    """Calculates each player's final score."""
+    final_scores = {}
+    score = 0
+    for player in dict:
+        if len(dict[player]) >= 1:
+            for card in dict[player]:
+                score += int(cards.card_points[card])
+        else:
+            score = 0
+        final_scores[player] = score
+    ranks = sorted(final_scores.items(), key= lambda c: c[1])
+    return ranks
+
+
+def print_ranks(list): #should use list returned by end_game func
+    """Prints out player place and their final score."""
+    place = 0
+    for player in list:
+        place += 1
+        print(f"{place}. {player[0]} with {player[1]} points")
