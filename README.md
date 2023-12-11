98168bb7774770554531c9ec8a01bf9c08d21cb1
Angela Boley <aboley@terpmail.umd.edu>
2023-12-11 13:55:40 -0500

    adding draw(), choice(), and wildChoice() function

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # inst_326_project
+| Method/Function | Primary Author | Techniques Demonstrated |
+| --------------- | -------------- | ----------------------- |
+| deck_info       | Erin Keane     | Cards Class
--- inst326_final.py
+++ inst326_final.py
@@ -5,7 +5,6 @@ import argparse
 import sys
 import re
 
-
 class Cards:
    """A class of Uno cards."""
    def __init__(self):
@@ -16,8 +15,7 @@ class Cards:
        """
        self.colors = ["Red", "Yellow", "Blue", "Green"]
        self.types = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Reverse', 'Draw2', 'Skip']
-       self.wild = ['Wild Card', 'Wild Draw4']
-       
+       self.wild = ['Wild Card', 'Wild Draw4'] 
 
 class Deck(Cards):
     """A class to make the deck of Uno cards."""
@@ -69,7 +67,6 @@ class Deck(Cards):
         """
         print(f"Deck: {self.deck}")
 
-
 def end_game(hands):
    """Calculates each player's final score.
  
@@ -84,7 +81,6 @@ def end_game(hands):
    ranks = sorted(final_scores.items(), key= lambda c: c[1])
    return ranks
 
-
 def print_ranks(hands):
    """Prints out player place and their final score.
  
@@ -97,7 +93,83 @@ def print_ranks(hands):
        place += 1
        print(f"{place}. Player {player[0] + 1} with {player[1]} points")
 
+def draw(shuffled_deck, discards, hands, turn, num_players=2):
+   """Draws a card from the deck.
+ 
+   Args:
+       shuffled_deck (list): a shuffled deck of Uno cards.
+       hands (dict): a dictionary containing the players' number (key) and
+           their hand of cards (value, a list).
+       turn (int): an index that represents whose turn it is.
+       num_players (int): number of players.
+       discards (list): the cards in the discard pile.
+ 
+   Side effects:
+       Prints to stdout.
+   """
+   print("You now have to draw from the deck.")
+   if len(shuffled_deck) == 0:
+       top_deck = discards.pop(0)
+       random.shuffle(discards)
+       shuffled_deck=shuffled_deck.extend(discards)
+       discards.clear()
+       discards.append(top_deck)
+       print(f"Drew a {shuffled_deck[0]}")
+       hands[turn].append(shuffled_deck.pop(0))
+       turn = (turn + 1) % num_players
+   else:
+       print(f"Drew a {shuffled_deck[0]}")
+       hands[turn].append(shuffled_deck.pop(0))
+       turn = (turn + 1) % num_players
+    
+def choice(num_cards):
+   """Validates the player's choice of playing or drawing a card.
+ 
+   Args:
+       num_cards (int): number of cards in a player's hand.
+ 
+   Side effects:
+       Prints to stdout.
+ 
+   Returns:
+       int: the number of the card the player wants to play.
+   """
+   while True:
+       card_number = input("What card would you like to play?\n"
+           "Select the card number.If you can't play a card, input 'draw'.")
+       if card_number == "draw":
+           return card_number
+       else:
+           try:
+               y = int(card_number)
+           except ValueError:
+               print("Please enter a number.")
+               continue
+           if 0 < y <= num_cards:
+               return y
+           else:
+               print("Pick a number in the specified range.")
 
+def wildChoice(choices):
+   """Player chooses which card they want to play when they have a wild card.
+ 
+   Args:
+       choices (int): the four color options the player can choose from
+     
+   Side effects:
+       Prints the options
+   """
+   pattern = r"^[1-4]$"
+   while True:
+       choice = input("What color do you want to change to? Select the number.")
+       if re.match(pattern, choice):
+           y = int(choice)
+           if y in choices:
+               return y
+           else:
+               print("Invalid choice. Please select a valid number.")
+       else:
+           print("Pick a number in the specified range.")
 
 def play(turn, hands, discards, direction, shuffled_deck, num_players):
    """Player taking a turn.
@@ -115,7 +187,6 @@ def play(turn, hands, discards, direction, shuffled_deck, num_players):
        Prints to stdout.
    """
    while True:
-     
        if len(hands[turn]) == 0:
            print(f"Player {turn + 1} won!")
            print_ranks(hands)
@@ -155,8 +226,6 @@ def play(turn, hands, discards, direction, shuffled_deck, num_players):
                        t= wildChoice(colors)
                        (hands[turn]).pop(int(x) -1)
                        discards.append(colors[int(t)])
-
-
                    elif "Wild Draw4" in selected_card:
                        colors = {1:"Blue Wild", 2:"Red Wild", 3:"Green Wild", 4:"Yellow Wild"}
                        for i in colors:
@@ -189,7 +258,6 @@ def main(num_players):
    while num_players < 2 or num_players > 4:
        num_players = int(input("Invalid. Please enter a number between 2-4. How many players? "))
 
-
    turn= 0
    direction = 1
    cards = Cards()
@@ -209,7 +277,6 @@ def main(num_players):
      
    play(turn, hands, discards, direction, shuffled_deck, num_players)
 
-
 def parse_args(arglist):
    """Parse and validate command-line arguments for Uno game.
   Args:
@@ -221,7 +288,6 @@ def parse_args(arglist):
    parser.add_argument('num_players', type=int, choices=range(2, 5),
                        help='Number of players (between 2 and 4)')
    return parser.parse_args(arglist)
-
 
 if __name__ == "__main__":
    args = parse_args(sys.argv[1:])
