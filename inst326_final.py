6089c4a2af1efc231926d8dcda9fafd0c6c56cff
Erin Keane <eringkeane@gmail.com>
2023-12-10 19:54:41 -0500

    final update from ek for final

--- inst326_final.py
+++ inst326_final.py
@@ -27,7 +27,6 @@ class Cards:
 
         color = ["Red", "Yellow", "Blue", "Green"]
         type = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Reverse', 'Draw2', 'Skip']
-        #type = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         wild = ['Wild Card', 'Wild Draw4']
         for colors in color:
             for types in type:
